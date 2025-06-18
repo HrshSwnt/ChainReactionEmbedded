a26c81a36509d12b726a5a1d9828bf00a71483f2
Harsh Sawant <hrsh.swnt@usask.ca>
2025-06-18 01:20:16 -0600

    Glitch fixed after fiing unlock location in code

--- ExplosionHandler.cpp
+++ ExplosionHandler.cpp
@@ -53,10 +53,11 @@ void ExplosionProcessorThread::processExplosions() {
     }
     if (!ExplosionQueue::instance().explosionQueue.empty()) {
         Explosion exp = ExplosionQueue::instance().explosionQueue.front(); // Get the first explosion from the queue
-        exp.cell->explode(exp.player); // Process the explosion in the cell   
         ExplosionQueue::instance().explosionQueue.pop(); // Remove the explosion from the queue
 
         lock.unlock(); // Unlock the mutex before processing the explosion
+        exp.cell->explode(exp.player); // Process the explosion in the cell   
+        
 
     }
     
